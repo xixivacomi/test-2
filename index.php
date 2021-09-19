e2b419dac137720ddae60824dc6862d97bb4b551
Your Name <xixivacomi@gmail.com>
2021-09-20 00:08:16 +0700

    thêm

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- heading -->
+    <div id="header">
+        <div class="header-img">
+            <img src="img/icon-shop.jpg" alt="icon-shop">
+        </div> 
+        <div class="header-nav">
+            <a href="#">Home</a>
+            <a href="#">Categories</a>
+            <a href="#">Foods</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+    <!-- slider -->
+    <div id="slider">
+        <img src="img/slider.jpg" alt="image-slider" class="slider-img">
+        <div class="slider-content ">
+            <input class="bo-rad" placeholder="search for food" type="text"> <a class="bo-rad" href="#"> Search</a></input>
+        </div>
+    </div>
+    <!-- main: content -->
+    <div id="content">
+        <!-- firstly section content -->
+        <div class="content-section clear">
+            <h1 class="main-header">Explore Foods</h1>
+            <div class="explore-list">
+                <div class="explore-cake">
+                    <img src="img/pizza.jpg" alt="" class="cake-img">
+                    <div class="sub-area">
+                        <p class="sub-explore-cake">Pizza</p>
+                    </div>
+                </div>
+                <div class="explore-cake">
+                    <img src="img/burger.jpg" alt="" class="cake-img">
+                    <div class="sub-area">
+                        <p class="sub-explore-cake">Burger</p>
+                    </div>
+                </div>
+                <div class="explore-cake">
+                    <img src="img/momo.jpg" alt="" class="cake-img">
+                    <div class="sub-area">
+                        <p class="sub-explore-cake">Momo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <!-- second section content -->
+        <div class="content-section">
+            <h2 class=".hear-menu">Food Menu</h2>
+        </div>
+
+    </div>
+    <!-- footer -->
+    <div id="footer"></div>
+</body>
+
+</html>
